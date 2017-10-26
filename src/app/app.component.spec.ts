b092a74273d221ee16263801e8b719e00097c814
fhfhfh <fmahon@redhat.com>
2017-10-26 08:31:22 +0100

    #testing Commit messages should be of the form  [CurrentSprint:TaskID] Commit Message.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,36 +3,52 @@ import { IonicModule } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { MyApp } from './app.component';
+// import { MyApp } from './app.component';
 
-describe('MyApp Component', () => {
-  let fixture;
-  let component;
+// import {
+//   PlatformMock,
+//   StatusBarMock,
+//   SplashScreenMock
+// } from '../../test-config/mocks-ionic';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [MyApp],
-      imports: [
-        IonicModule.forRoot(MyApp)
-      ],
-      providers: [
-        StatusBar,
-        SplashScreen
-      ]
-    })
-  }));
+// describe('MyApp Component', () => {
+//   let fixture;
+//   let component;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MyApp);
-    component = fixture.componentInstance;
-  });
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [MyApp],
+//       imports: [
+//         IonicModule.forRoot(MyApp)
+//       ],
+//       providers: [
+//         StatusBar,
+//         SplashScreen
+//       ]
+//     })
+//   }));
 
-  it ('should be created', () => {
-    expect(component instanceof MyApp).toBe(true);
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(MyApp);
+//     component = fixture.componentInstance;
+//   });
 
-  it ('should have two pages', () => {
-    expect(component.pages.length).toBe(2);
-  });
+//   it ('should be created', () => {
+//     expect(component instanceof MyApp).toBe(true);
+//   });
 
-});
+//   it ('should have two pages', () => {
+//     expect(component.pages.length).toBe(2);
+//   });
+
+// });
+
+describe('My Service', () => {
+  
+     it('should correctly add numbers', () => {
+  
+         expect(1 + 1).toBe(2);
+  
+     });
+  
+ });
