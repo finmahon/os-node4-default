6fc83a3084469e6551025d13c1c9c3f84c844f8a
fhfhfh <fmahon@redhat.com>
2017-10-31 10:16:23 +0000

    #testing Commit messages should be of the form  [CurrentSprint:TaskID] Commit Message.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,45 +3,45 @@ import { IonicModule } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-// import { MyApp } from './app.component';
+import { MyApp } from './app.component';
 
-// import {
-//   PlatformMock,
-//   StatusBarMock,
-//   SplashScreenMock
-// } from '../../test-config/mocks-ionic';
+import {
+  PlatformMock,
+  StatusBarMock,
+  SplashScreenMock
+} from '../../test-config/mocks-ionic';
 
-// describe('MyApp Component', () => {
-//   let fixture;
-//   let component;
+describe('MyApp Component', () => {
+  let fixture;
+  let component;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [MyApp],
-//       imports: [
-//         IonicModule.forRoot(MyApp)
-//       ],
-//       providers: [
-//         StatusBar,
-//         SplashScreen
-//       ]
-//     })
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MyApp],
+      imports: [
+        IonicModule.forRoot(MyApp)
+      ],
+      providers: [
+        StatusBar,
+        SplashScreen
+      ]
+    })
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(MyApp);
-//     component = fixture.componentInstance;
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyApp);
+    component = fixture.componentInstance;
+  });
 
-//   it ('should be created', () => {
-//     expect(component instanceof MyApp).toBe(true);
-//   });
+  it ('should be created', () => {
+    expect(component instanceof MyApp).toBe(true);
+  });
 
-//   it ('should have two pages', () => {
-//     expect(component.pages.length).toBe(2);
-//   });
+  it ('should have two pages', () => {
+    expect(component.pages.length).toBe(2);
+  });
 
-// });
+});
 
 describe('My Service', () => {
   
